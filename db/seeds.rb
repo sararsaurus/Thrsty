87f7fd917fdb917c3ab03b6019c963ccfd420833
Sara Raley <sara.m.raley@gmail.com>
2022-12-23 15:29:58 -0700

    adds link to good resources in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@ CSV.foreach(Rails.root.join("lib/breweries_us.csv"), headers: true) do |row|
     website: row[3],
   })
 end
+
+# https://itnext.io/how-to-seed-a-rails-database-with-a-csv-file-105a7ba0e88c
