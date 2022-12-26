4bbbe9dda29153c9d4c545920122a59c3c895f2c
hannahekehoe <hkehoe90@gmail.com>
2022-12-26 11:46:58 -0600

    added users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/breweries" => "breweries#index"
+  post "/users" => "users#create"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_23_212750) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_26_173820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_23_212750) do
     t.string "website"
     t.string "state"
     t.string "state_breweries"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
